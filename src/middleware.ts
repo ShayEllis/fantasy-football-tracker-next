343b9907708a31b43fb67c3c1a6c6e9a7c695926
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-08-26 15:58:21 -0700

    added db drivers for edge runtime

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,18 @@
 // Need Prisma edge drivers for this - same with route.ts 'edge' settings
+export const runtime = 'edge'
 export { auth as middleware } from './auth/auth'
 
 // import { NextRequest } from "next/server";
 // import { NextResponse } from "next/server";
 // import { auth } from "./auth/auth";
 
-
 // export async function middleware(request: NextRequest)  {
-  // const session = await auth()
+// const session = await auth()
 
-  // if (!session) {
-  //     console.log('********* REDIRECT ************')
-  //     return NextResponse.redirect(new URL('/signin', request.nextUrl.origin))
-  // }
-  
-  
+// if (!session) {
+//     console.log('********* REDIRECT ************')
+//     return NextResponse.redirect(new URL('/signin', request.nextUrl.origin))
+// }
 
-  // console.log('GOOD!')
+// console.log('GOOD!')
 // }
