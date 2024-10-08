90602d7a17ba27aafd791c8b332223dc3fecf070
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2024-10-07 18:42:02 -0700

    added functionality to all leagues table

--- src/components/Tables/AllLeaguesStatsTable/AllLeaguesColumns.tsx
+++ src/components/Tables/AllLeaguesStatsTable/AllLeaguesColumns.tsx
@@ -1,14 +1,13 @@
-import { ColumnDef, createColumnHelper } from '@tanstack/react-table'
-import { type SingleLeagueWeek } from '../SingleLeagueStatsTable/SingleLeagueColumns'
+import { createColumnHelper } from '@tanstack/react-table'
 
 export type League = {
   leagueName: string
-  week: number
-  projectedWin: number
-  win: boolean
-  projectedPoints: number
-  points: number
-  place: number
+  week?: number
+  projectedWin?: number
+  win?: boolean
+  projectedPoints?: number
+  points?: number
+  place?: number
 }
 
 // Example data
@@ -36,7 +35,7 @@ export const allLeaguesTestData = [
     leagueName: 'league 2',
     week: 1,
     projectedWin: 2,
-    win: true,
+    win: false,
     projectedPoints: 22,
     points: 2,
     place: 5,
@@ -48,9 +47,8 @@ export const allLeaguesTestData = [
     win: true,
     projectedPoints: 22,
     points: 2,
-    place: 5,
+    place: 55,
   },
-
   {
     leagueName: 'league 3',
   },
@@ -61,32 +59,23 @@ const columnHelper = createColumnHelper<League>()
 export const allLeaguesColumns = [
   columnHelper.accessor('leagueName', {
     header: () => <span>League</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
   columnHelper.accessor('week', {
     header: () => <span>Week</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
   columnHelper.accessor('projectedWin', {
     header: () => <span>Proj. Win</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
   columnHelper.accessor('win', {
     header: () => <span>Win</span>,
-    cell: (info) => (
-      <div className='text-center'>{info.getValue()?.toString()}</div>
-    ),
   }),
   columnHelper.accessor('projectedPoints', {
     header: () => <span>Proj. Points</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
   columnHelper.accessor('points', {
     header: () => <span>Points</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
   columnHelper.accessor('place', {
     header: () => <span>Place</span>,
-    cell: (info) => <div className='text-center'>{info.getValue()}</div>,
   }),
 ]
--- src/components/Tables/AllLeaguesStatsTable/AllLeaguesStatsTable.tsx
+++ src/components/Tables/AllLeaguesStatsTable/AllLeaguesStatsTable.tsx
@@ -1,82 +1,178 @@
 'use client'
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getExpandedRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import {
-  Table,
+  Column,
+  ColumnDef,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  flexRender,
+  RowData,
+  SortingFn,
+  FilterFn,
+  sortingFns,
+  PartialKeys,
+  TableOptionsResolved,
+  type Table,
+} from '@tanstack/react-table'
+import {
+  Table as TableElement,
   TableBody,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { ArrowDown } from 'lucide-react'
-import { cn } from '@/lib/utils'
+import { Input } from '@/components/ui/input'
+
+import {
+  RankingInfo,
+  rankItem,
+  compareItems,
+} from '@tanstack/match-sorter-utils'
 
 export { allLeaguesColumns, type League } from './AllLeaguesColumns'
+
+declare module '@tanstack/react-table' {
+  interface TableMeta<TData extends RowData> {
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void
+  }
+  interface TableOptions<TData extends RowData>
+    extends PartialKeys<
+      TableOptionsResolved<TData>,
+      'state' | 'onStateChange' | 'renderFallbackValue'
+    > {
+    filterFns?: FilterFns
+  }
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo
+  }
+}
+
+// Define a custom fuzzy filter function that will apply ranking info to rows (using match-sorter utils)
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value)
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  })
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed
+}
+
+// Define a custom fuzzy sort function that will sort by rank if the row has ranking information
+const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
+  let dir = 0
+
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    )
+  }
+
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
+}
+
+function useSkipper() {
+  const shouldSkipRef = useRef(true)
+  const shouldSkip = shouldSkipRef.current
+
+  // Wrap a function with this to skip a pagination reset temporarily
+  const skip = useCallback(() => {
+    shouldSkipRef.current = false
+  }, [])
+
+  useEffect(() => {
+    shouldSkipRef.current = true
+  })
+
+  return [shouldSkip, skip] as const
+}
 
 type DataTableProps<TData, TValue> = {
   data: TData[]
   columns: ColumnDef<TData, TValue | any>[]
 }
 
-const defaultColumn: Partial<ColumnDef<League>> = {
-  cell: ({ getValue, row: { index }, column: { id }, table }) => {
-    const initialValue = getValue()
-    // We need to keep and update the state of the cell normally
-    const [value, setValue] = useState(initialValue)
-
-    // When the input is blurred, we'll call our table meta's updateData function
-    const onBlur = () => {
-      table.options.meta?.updateData(index, id, value)
-    }
-
-    // If the initialValue is changed external, sync it up with our state
-    useEffect(() => {
-      setValue(initialValue)
-    }, [initialValue])
-
-    return (
-      <input
-        value={value as string}
-        onChange={e => setValue(e.target.value)}
-        onBlur={onBlur}
-      />
-    )
-  },
-}
-
 export function AllLeaguesStatsTable<TData, TValue>({
   data,
   columns,
 }: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = useState<SortingState>([])
+  const [tableData, setTableData] = useState(data)
+  const [globalFilter, setGlobalFilter] = useState('')
+  const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper()
+
+  useEffect(() => {
+    console.log('all leagues data', tableData)
+  }, [tableData])
+
+  // Give our default column cell renderer editing superpowers!
+  const defaultColumn: Partial<ColumnDef<TData>> = {
+    cell: function TableCell({
+      getValue,
+      row: { index },
+      column: { id },
+      table,
+    }) {
+      const initialValue = getValue()
+      // We need to keep and update the state of the cell normally
+      const [value, setValue] = useState(initialValue)
+
+      // When the input is blurred, we'll call our table meta's updateData function
+      const onBlur = () => {
+        table.options.meta?.updateData(index, id, value)
+      }
+
+      // If the initialValue is changed external, sync it up with our state
+      useEffect(() => {
+        setValue(initialValue)
+      }, [initialValue])
+
+      return (
+        <Input
+          className='text-center p-0 h-auto bg-transparent'
+          value={value as string}
+          onChange={(e) => setValue(e.target.value)}
+          onBlur={onBlur}
+        />
+      )
+    },
+  }
+
   const table = useReactTable({
     data,
     columns,
-    initialState: {},
-    debugTable: true,
-    getCoreRowModel: getCoreRowModel(),
-    onSortingChange: setSorting,
-    getSortedRowModel: getSortedRowModel(),
-    state: {
-      sorting,
+    filterFns: {
+      fuzzy: fuzzyFilter,
     },
+    defaultColumn,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    globalFilterFn: 'fuzzy',
+    onGlobalFilterChange: setGlobalFilter,
+    state: {
+      globalFilter,
+    },
+    getPaginationRowModel: getPaginationRowModel(),
+    autoResetPageIndex,
+    // Provide our updateData function to our table meta
     meta: {
       updateData: (rowIndex, columnId, value) => {
         // Skip page index reset until after next rerender
         skipAutoResetPageIndex()
-        setData(old =>
+        setTableData((old) =>
           old.map((row, index) => {
             if (index === rowIndex) {
               return {
@@ -89,54 +185,37 @@ export function AllLeaguesStatsTable<TData, TValue>({
         )
       },
     },
-    // enableMultiSort: false, //Don't allow shift key to sort multiple columns - default on/true
-    // ***** Can hold shift to sort on multiple columns ******
+    debugTable: true,
   })
 
-  console.log('sorting', sorting)
-
-  //access sorting state from the table instance
-  console.log(table.getState().sorting.length)
-  console.log(table.getState().sorting)
-
   return (
-    <div className='rounded-md border'>
-      <Table>
+    <div className='p-2'>
+      <div>
+        <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={(value) => setGlobalFilter(String(value))}
+          className='p-2 font-lg shadow border border-block'
+          placeholder='Search all columns...'
+        />
+      </div>
+      <TableElement>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead key={header.id} colSpan={header.colSpan}>
                     {header.isPlaceholder ? null : (
-                      <div className='text-center'>
-                        <Button
-                          variant='ghost'
-                          className='relative'
-                          title={
-                            header.column.getCanSort()
-                              ? header.column.getNextSortingOrder() === 'asc'
-                                ? 'Sort ascending'
-                                : header.column.getNextSortingOrder() === 'desc'
-                                ? 'Sort descending'
-                                : 'Clear sort'
-                              : undefined
-                          }
-                          onClick={header.column.getToggleSortingHandler()}>
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                          {header.column.getIsSorted() === 'asc'}
-                          <ArrowDown
-                            className={cn(
-                              'size-4 absolute right-0',
-                              header.column.getIsSorted() === false && 'hidden',
-                              header.column.getIsSorted() === 'desc' &&
-                                'rotate-180'
-                            )}
-                          />
-                        </Button>
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <Filter column={header.column} table={table} />
+                          </div>
+                        ) : null}
                       </div>
                     )}
                   </TableHead>
@@ -146,27 +225,167 @@ export function AllLeaguesStatsTable<TData, TValue>({
           ))}
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && 'selected'}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className='h-12 text-center'>
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
+            )
+          })}
         </TableBody>
-      </Table>
+      </TableElement>
+      <div className='h-2' />
+      <div className='flex items-center gap-2'>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}>
+          {'<<'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}>
+          {'<'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}>
+          {'>'}
+        </button>
+        <button
+          className='border rounded p-1'
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}>
+          {'>>'}
+        </button>
+        <span className='flex items-center gap-1'>
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className='flex items-center gap-1'>
+          | Go to page:
+          <input
+            type='number'
+            min='1'
+            max={table.getPageCount()}
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className='border p-1 rounded w-16'
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value))
+          }}>
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>{table.getRowModel().rows.length} Rows</div>
     </div>
+  )
+}
+function Filter({
+  column,
+  table,
+}: {
+  column: Column<any, any>
+  table: Table<any>
+}) {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className='flex space-x-2'>
+      <input
+        type='number'
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className='w-24 border shadow rounded'
+      />
+      <input
+        type='number'
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={(e) =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className='w-24 border shadow rounded'
+      />
+    </div>
+  ) : (
+    <input
+      type='text'
+      value={(columnFilterValue ?? '') as string}
+      onChange={(e) => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className='w-36 border shadow rounded'
+    />
+  )
+}
+
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
   )
 }
